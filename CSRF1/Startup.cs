a66f332eecd47d2a12dd20f798f37ab5686782e6
Gangabyraiah <GangabyraiahM@michigan.gov>
2018-11-08 10:06:15 -0500

    Add project files.

---
+++ CSRF1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSRF1.Startup))]
+namespace CSRF1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

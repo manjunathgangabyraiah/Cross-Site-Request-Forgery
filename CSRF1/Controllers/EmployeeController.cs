6fd3aa5db7c05180d388ff24e01a0519d982a9b7
Manjunath <mgangaby@gmail.com>
2019-04-19 08:17:52 -0400

    First commit message

---
+++ CSRF1/Controllers/EmployeeController.cs
@@ -0,0 +1,147 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Modelbinding1.Controllers
+{
+	public class EmployeeController : Controller
+	{
+		public ActionResult Index()
+		{
+			EmployeeBusinessLayer employeeBusinessLayer =
+				new EmployeeBusinessLayer();
+
+			List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+			return View(employees);
+		}
+
+		[HttpGet]
+		[ValidateInput(false)]
+		[Authorize]
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+
+
+		[HttpPost]
+		[ActionName("Create")]
+		[Authorize]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create_Post()
+		{
+			Employee employee = new Employee();
+			TryUpdateModel<Employee>(employee);
+
+			if (ModelState.IsValid)
+			{
+				EmployeeBusinessLayer employeeBusinessLayer =
+					new EmployeeBusinessLayer();
+				employeeBusinessLayer.AddEmmployee(employee);
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		
+
+		[HttpGet]
+		public ActionResult Edit(int id)
+		{
+			EmployeeBusinessLayer employeeBusinessLayer =
+				   new EmployeeBusinessLayer();
+			Employee employee =
+				   employeeBusinessLayer.Employees.Single(emp => emp.ID == id);
+
+			return View(employee);
+		}
+
+
+
+		[HttpPost]
+		[ActionName("Edit")]
+		public ActionResult Edit_Post(int id)
+		{
+			EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+
+			Employee employee = employeeBusinessLayer.Employees.Single(x => x.ID == id);
+			TryUpdateModel(employee);
+
+			if (ModelState.IsValid)
+			{
+				employeeBusinessLayer.SaveEmployee(employee);
+
+				return RedirectToAction("Index");
+			}
+
+			return View(employee);
+		}
+
+
+		public ActionResult Delete(int id)
+		{
+			EmployeeBusinessLayer employeeBusinessLayer =
+				new EmployeeBusinessLayer();
+			employeeBusinessLayer.DeleteEmployee(id);
+			return RedirectToAction("Index");
+		}
+
+		//[HttpPost]
+		//[ActionName("Create")]
+		//public ActionResult Create_Post(Employee employee)
+		//{
+		//	EmployeeBusinessLayer employeeBusinessLayer =
+		//		new EmployeeBusinessLayer();
+
+		//	if (ModelState.IsValid)
+		//	{
+		//		employeeBusinessLayer.AddEmmployee(employee);
+		//		return RedirectToAction("Index");
+		//	}
+		//	else
+		//	{
+		//		return View();
+		//	}
+		//}
+
+
+		//[HttpPost]
+		//public ActionResult Create(FormCollection formCollection)
+		//{
+		//	Employee employee = new Employee();
+		//	// Retrieve form data using form collection
+		//	employee.Name = formCollection["Name"];
+		//	employee.Gender = formCollection["Gender"];
+		//	employee.City = formCollection["City"];
+		//	employee.DateOfBirth =
+		//		Convert.ToDateTime(formCollection["DateOfBirth"]);
+
+		//	EmployeeBusinessLayer employeeBusinessLayer =
+		//		new EmployeeBusinessLayer();
+
+		//	employeeBusinessLayer.AddEmmployee(employee);
+		//	return RedirectToAction("Index");
+		//}
+
+		//[HttpPost]
+		//public ActionResult Create(string name, string gender, string city, DateTime dateOfBirth)
+		//{
+		//	Employee employee = new Employee();
+		//	employee.Name = name;
+		//	employee.Gender = gender;
+		//	employee.City = city;
+		//	employee.DateOfBirth = dateOfBirth;
+
+		//	EmployeeBusinessLayer employeeBusinessLayer =
+		//		new EmployeeBusinessLayer();
+
+		//	employeeBusinessLayer.AddEmmployee(employee);
+		//	return RedirectToAction("Index");
+		//}
+
+	}
+}
---
+++ ClassLibrary1/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+
+namespace BusinessLayer
+{
+	[ValidateAntiForgeryToken]
+	public class Employee
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+		public string Gender { get; set; }
+
+
+		[Required]
+
+
+		[StringLength(30, ErrorMessage = "The {0} must be atleast {2} characters long.", MinimumLength = 6)]
+		public string City { get; set; }
+		[Required]
+		public DateTime? DateOfBirth { get; set; }
+
+
+	}
+
+
+	public class CustomValidationclass : ValidationAttribute
+	{
+		public static ValidationResult Compare(string value)
+		{
+
+			if (value == "Chicago")
+				return ValidationResult.Success;
+			else
+				return new ValidationResult("the value is not good");
+		}
+	}
+
+}
